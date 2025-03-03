c96d955f353468f4c0542ba47c44c3363783b15e
MatheusARPro <105962661+MatheusARPro@users.noreply.github.com>
2025-03-03 01:00:46 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def efeito_digitacao(texto, intervalo=0.05):
-    for letra in texto:
-        print(letra, end="", flush=True)
-        time.sleep(intervalo)
-    print()  # Pula linha no final
-
-efeito_digitacao("Olá, este texto será digitado aos poucos!")
